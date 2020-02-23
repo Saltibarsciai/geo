4a58ad5fa902479753d1c4559ddc93ad3f2470e4
Saltibarsciai <rakunas.dominykas@gmail.com>
2020-02-23 18:03:06 +0200

    Updates

---
+++ precache-manifest.d2e8fe2f3111b58c2894543cb9b5e379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a65436ba6322b4c4130f845df1fc26",
+    "url": "/geo/index.html"
+  },
+  {
+    "revision": "66891cd1e5f63bf4ef00",
+    "url": "/geo/static/css/2.699c7d85.chunk.css"
+  },
+  {
+    "revision": "58702a47122634e8ca71",
+    "url": "/geo/static/css/main.f6c8e2dd.chunk.css"
+  },
+  {
+    "revision": "66891cd1e5f63bf4ef00",
+    "url": "/geo/static/js/2.e177969a.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/geo/static/js/2.e177969a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58702a47122634e8ca71",
+    "url": "/geo/static/js/main.a8a363bf.chunk.js"
+  },
+  {
+    "revision": "f0b843d407835a418b1e",
+    "url": "/geo/static/js/runtime-main.56c59af1.js"
+  }
+]);
